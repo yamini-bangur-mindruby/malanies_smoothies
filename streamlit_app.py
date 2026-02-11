fcf7bcb5d79a619ec0eb0b66cab0d81c423d5c30
yamini-bangur-mindruby <117260053+yamini-bangur-mindruby@users.noreply.github.com>
2026-02-11 12:45:32 +0530

    Fix SQL insert statement and remove duplicate API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,15 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen+ ' '
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!', icon="✅")
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
+
 
 
